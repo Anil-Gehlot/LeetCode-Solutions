7665e9aa9258672afe9b5a099212b5bed3ebb474
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-18 16:08:41 +0530

    Time: 0 ms (100%), Space: 40.5 MB (88.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        //  to hold the result of x^n
+        double result = Math.pow(x, n);
+        
+        return result;
+    }
+    
+}
