e548be6a7f00910e586a003e12c151e5d313aeab
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-18 21:58:54 +0530

    Time: 39 ms (84.1%), Space: 16.3 MB (30.01%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,18 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
+        # to hold the number
         sum = 0
         for i in range(0, len(digits)):
             sum = (sum*10 ) + digits[i]
 
+        # increasing value by one according to condition
         sum +=1
+
+        # to return the increased value in the form of array
         list1 = []
+
+        # holding index to insert the element 
         index = 0
         while(sum != 0 ):
             last_digit = sum % 10
@@ -14,5 +20,6 @@ class Solution:
             index += 1
             sum = sum//10
         
+        # returning new list
         return list1
 
