37c25f1173bd3c13648b99fac0c97ea2822c362a
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-08-25 15:47:19 +0530

    Time: 37 ms (75.4%), Space: 16.2 MB (97.83%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
+        # dictionary for taking values of numbers....
         dict1 ={
                     "2" : "abc",
                     "3" : "def",
@@ -11,36 +12,44 @@ class Solution:
                     "8" : "tuv", 
                     "9" : "wxyz"
                 }
+        
+        # length of given digit
         length = len(digits)
 
+        # final list to return the result
         final = []
 
+        # if there is no digits
         if digits == "":
             # return []
             print([])
 
+        # if there are 1 digit
         if length == 1:
             for i in dict1[digits]:
                 final.append(i)
 
+        # if there are 2 digits 
         if length == 2 :
             for i in dict1[digits[0]]:
                 for j in dict1[digits[1]]:
                     final.append(i+j)
 
-
+        # if there are 3 digits
         if length == 3 :
             for i in dict1[digits[0]]:
                 for j in dict1[digits[1]]:
                     for k in dict1[digits[2]]:
                         final.append(i+j+k)
-
+        # if there are 4 digits
         if length == 4 :
             for i in dict1[digits[0]]:
                 for j in dict1[digits[1]]:
                     for k in dict1[digits[2]]:
                         for l in dict1[digits[3]]:
                             final.append(i+j+k+l)
+                            
+        # to return final list
         return final
 
 
