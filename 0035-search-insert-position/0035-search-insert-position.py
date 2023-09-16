462ed030664867d98fdffc156767daa37d739d7d
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-09-16 20:52:44 +0530

    Time: 58 ms (41.67%), Space: 17.2 MB (52.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,14 +1,16 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
+        # finding the index if target is present inside the nums
         for number in nums:
             if number == target:
                 return nums.index(number)
             
-        
+        # finding a index where target shuold be placed
         for number in nums:
             if number > target:
                 return nums.index(number)
-              
+
+        # target should be placed at the end   
         return len(nums)
         
