9bb9990297db4f57ca6b1f4bbaebec88a2b6819a
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-10-03 09:36:27 +0530

    Time: 50 ms (5.92%), Space: 16.4 MB (18.82%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,13 +1,19 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
 
+        # empty list to store the equal numbers indexes.
         nums1 = []
+
+        # outer loop to traverse all numbers excluding last.
         for i in range(0, len(nums)-1):
-    
+            
+            # inner loop to traverse all numbers excluding last.
             for j in range(i+1, len(nums)):
-        
+                
+                # comparing numbers and adding in nums1
                 if nums[i] == nums[j]:
                     nums1.append( [i,j] )
 
+        #returning the count of matched numbers
         return len(nums1)
         
