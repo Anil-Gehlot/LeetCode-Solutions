d23b2b1ecad289e7a52b9f02b1a49b66d678769d
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-10-07 14:50:36 +0530

    Time: 42 ms (43.49%), Space: 16.1 MB (77.84%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,14 +1,15 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-
-        # to return False for negative values
-        if n <= 0:
-            return False
         
-        while n%2 == 0:
-            n = n//2
-        
-        if n==1:
+        # Check if n is equal to 1 or n divided by 2 is equal to 1
+        # This is to handle the case when n is a power of two (e.g., 1, 2, 4, 8, ...)
+        if n / 2 == 1 or n == 1:
             return True
+
+        # If n is greater than 2 and divisible by 2,
+        # recursively call isPowerOfTwo with n/2
+        if n > 2:
+            return self.isPowerOfTwo(n/2)
         else:
+            # If n is not a power of two, return False
             return False
