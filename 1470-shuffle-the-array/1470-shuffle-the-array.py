a86bbe699275ce8d84528e9fdee96659909cc036
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-11-08 22:22:22 +0530

    Time: 67 ms (23.04%), Space: 16.4 MB (73.09%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,16 +1,23 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         
+        # Initialize an empty list to store the rearranged elements.
         new_nums = []
         
-        i = n
+        # Initialize 'mid' to 'n' since 'mid' will track the index in the second half of 'nums'.
+        mid = n
         
+        # Iterate over a range from 0 to 'n-1' (inclusive).
         for number in range(n):
+            
+            # Append the element from the first half of 'nums' to 'new_nums'.
             new_nums.append(nums[number])
-            new_nums.append(nums[i])
-            i += 1
             
+            # Append the element from the second half of 'nums' to 'new_nums'.
+            new_nums.append(nums[mid])
+            
+            # Increment 'mid' to move to the next element in the second half of 'nums'.
+            mid += 1
+            
+        # Return the 'new_nums' list, which contains the rearranged elements.
         return new_nums
-            
-            
-        
