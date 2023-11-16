6399ad0ebf9fda592d71fed511de39b75f23aa1c
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2023-11-16 11:34:51 +0530

    Time: 42 ms (57.78%), Space: 16.4 MB (42.98%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,5 +1,63 @@
+from typing import List
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        
+        # Get the length of the binary strings in the array
+        n = len(nums)
+
+        # Create a binary string of all ones and all zeros with length n
+        all_ones = "1" * n
+        all_zeros = "0" * n
+
+        # Check if the binary string of all ones is not in nums, return it
+        if all_ones not in nums:
+            return all_ones
+
+        # Check if the binary string of all zeros is not in nums, return it
+        if all_zeros not in nums:
+            return all_zeros
+        
+        # Iterate through each binary string in nums
+        for binary_str in nums:
+            # Check if the reverse of the current string is not in nums, return the reversed string
+            if binary_str[::-1] not in nums:
+                return binary_str[::-1]
+        
+        # last case: If none of the above conditions are met, return "10"
+        return "10"
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
 
         # Handling the case where the array has only one element
         if len(nums)==1 and "0" not in nums:
@@ -23,3 +81,5 @@ class Solution:
         
         # last case 
         return "10"
+        
+        '''
