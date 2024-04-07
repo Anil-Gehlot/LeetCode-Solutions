90f0160642ea8315e20f298c4d7fe5c8f0682228
Anil Gehlot <119098076+Anil-Gehlot@users.noreply.github.com>
2024-04-07 07:46:20 +0530

    Time: 58 ms (18.05%), Space: 16.6 MB (90.66%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,18 +1,14 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Create an empty list to store the intersection of nums1 and nums2.
-        final_list = []
-        
-        # Iterate through each number in nums1.
-        for number in nums1:
-            # Check if the current number is also present in nums2.
-            if number in nums2:
-                # If the number is found in nums2, add it to the final_list.
-                final_list.append(number)
-                # Remove the number from nums2 to avoid duplicate entries.
-                nums2.remove(number)
-        
-        # Return the final_list containing the intersection of nums1 and nums2.
-        return final_list
+
+        result = []
+
+        for val in nums1 :
+            if val in nums2:
+                result.append(val)
+                nums2.remove(val)
+                
+        return result
+
 
         
